17465a32b0924d1a10746a2305ce2a1545922fe9
JoaquimGCVS <jgui9292@gmail.com>
2025-04-30 18:10:43 -0300

    adicionando getters e setters para model admin

--- Back-end/estoque/src/main/java/com/orthomoveis/estoque/model/Admin.java
+++ Back-end/estoque/src/main/java/com/orthomoveis/estoque/model/Admin.java
@@ -16,5 +16,19 @@ public class Admin {
         this.password = password;
     }
 
-    // Getters e Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
